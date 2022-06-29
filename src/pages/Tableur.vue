4489ea64f87dbcb79a06ca4735ca8820a45c6541
Waylander <90508325+LordWaylander@users.noreply.github.com>
2022-06-29 17:25:46 +0200

    Update Tableur.vue

--- src/pages/Tableur.vue
+++ src/pages/Tableur.vue
@@ -4,22 +4,22 @@
      <div>
         <form v-on:submit.prevent="submit($event)">
             <label for="coursAchatAction">Cours de l'action à l'achat</label>
-            <input type="text" placeholder="Cout action à l'achat" name="coursAchatAction"/>
+            <input type="text" placeholder="Cout action à l'achat" name="coursAchatAction" v-model="form.coursAchatAction"/>
             <label for="quantite">Quantité</label>
-            <input type="text" placeholder="quantité" name="quantite"/>
+            <input type="text" placeholder="quantité" name="quantite" v-model="form.quantite"/>
             <label for="fraisAchat">Frais d'achat</label>
-            <input type="text" placeholder="frais d'achat" name="fraisAchat"/>
+            <input type="text" placeholder="frais d'achat" name="fraisAchat" v-model="form.fraisAchat"/>
             <label for="fraisVente">Frais de vente</label>
-            <input type="text" placeholder="frais de vente" name="fraisVente"/>
+            <input type="text" placeholder="frais de vente" name="fraisVente" v-model="form.fraisVente"/>
             <label for="coursActionActuelle">Cours de l'action à l'instant</label>
-            <input type="text" placeholder="Cours action actuelle" name="coursActionActuelle"/>
+            <input type="text" placeholder="Cours action actuelle" name="coursActionActuelle" v-model="form.coursActionActuelle"/>
             <div id="checkbox">
-                <label for="USD" id="USDcheck">USD ?</label>
-                <input type="checkbox" id="USDinput" @click="changeUSD($event)"/>
+                <label for="USDinput" id="USDcheck">USD ?</label>
+                <input type="checkbox" id="USDinput" v-model="form.USDinput"/>
             </div>
-            <div v-if="deviseUSDCheckbox" id="USD">
+            <div v-if="form.USDinput" id="USD">
                 <label for="coursDollar">Cours du dollar</label>
-                <input type="text" name="coursDollar" placeholder="Cours du Dollar"/>
+                <input type="text" name="coursDollar" placeholder="Cours du Dollar" v-model="form.coursDollar"/>
             </div>
             <input type="submit" value="Envoyer" />
         </form>        
@@ -31,12 +31,12 @@
             <p><span class="coutTotalAction">Cout Total / action : </span>{{coutTotalAction}} $</p>
             <p><span class="investissementTotal">Investissement total SANS LES FRAIS DE VENTE: </span>{{investissementTotal}} $</p>
             <p><span class="investissementTotalApresVente">Investissement total apres vente : </span>{{investissementTotalApresVente}} $</p>
-            <p><span class="plusValue">+/- value : </span>{{plusValue}} $</p>
-            <p><span class="deviseUSD">Taux de change USD : </span>{{deviseUSDValue}}</p>
+            <p><span class="plusValue">+/- value : </span>{{plusValue}} €</p>
+            <p><span class="deviseUSD">Taux de change USD : </span>{{form.coursDollar}}</p>
             
-            <p class="v0">{{value0}} $</p> 
-            <p class="v50">{{value50}} $</p>
-            <p class="v100">{{value100}} $</p>
+            <p class="v0"><span>Prix de rachat action à +value 0% : </span>{{value0}} $</p> 
+            <p class="v50"><span>Prix de rachat action à +value 50% : </span>{{value50}} $</p>
+            <p class="v100"><span>Prix de rachat action à +value 100% : </span>{{value100}} $</p>
         </div>
         <div v-else-if="deviseUSD === false" class="values">
             <p><span class="coutTotalAchat">Cout Total d'achat : </span>{{coutTotalAchat}} €</p>
@@ -63,6 +63,15 @@ export default {
     },
     data(){
         return{
+            form: {
+                coursAchatAction: '0',
+                quantite: '0',
+                fraisAchat: '0',
+                fraisVente: '0',
+                coursActionActuelle: '0',
+                USDinput: false,
+                coursDollar: '0',
+            },
             coutTotalAchat:'',
             coutTotalAction:'',
             investissementTotal:'',
@@ -72,50 +81,46 @@ export default {
             value50: '',
             value100: '',
             deviseUSD: '',
-            deviseUSDCheckbox: false,
-            deviseUSDValue: 1.0534
+            //deviseUSDCheckbox: false,
         } 
     },
     methods: {
         submit(e) {
             this.deviseUSD = e.target[5].checked;
-            let coursAchatAction = parseFloat(e.target[0].value);
-            let quantite = parseInt(e.target[1].value, 10);
-            let fraisAchat = parseFloat(e.target[2].value);
-            let fraisVente =  parseFloat(e.target[3].value);
-            let coursAction =  parseFloat(e.target[4].value);
+            this.form.coursAchatAction = parseFloat(this.form.coursAchatAction);
+            this.form.quantite = parseInt(this.form.quantite);
+            this.form.fraisAchat = parseFloat(this.form.fraisAchat);
+            this.form.fraisVente =  parseFloat(this.form.fraisVente);
+            this.form.coursActionActuelle =  parseFloat(this.form.coursActionActuelle);
+            this.form.coursDollar = parseFloat(this.form.coursDollar);
 
             if (!this.deviseUSD) {
-                this.coutTotalAchat = (coursAchatAction*quantite)+fraisAchat;
-                this.coutTotalAction = this.coutTotalAchat/quantite;
-                this.investissementTotal = quantite*coursAction
-                this.investissementTotalApresVente = this.investissementTotal-fraisVente
+                this.coutTotalAchat = (this.form.coursAchatAction*this.form.quantite)+this.form.fraisAchat;
+                this.coutTotalAction = this.coutTotalAchat/this.form.quantite;
+                this.investissementTotal = this.form.quantite*this.form.coursActionActuelle
+                this.investissementTotalApresVente = this.investissementTotal-this.form.fraisVente
                 this.plusValue = this.investissementTotalApresVente-this.coutTotalAchat
 
                 
-                this.value0 = (this.investissementTotalApresVente-(this.plusValue*(0/100)))/quantite
-                this.value50 = (this.investissementTotalApresVente-(this.plusValue*(50/100)))/quantite;
-                this.value100 = (this.investissementTotalApresVente-this.plusValue)/quantite;
+                this.value0 = (this.investissementTotalApresVente-(this.plusValue*(0/100)))/this.form.quantite
+                this.value50 = (this.investissementTotalApresVente-(this.plusValue*(50/100)))/this.form.quantite;
+                this.value100 = (this.investissementTotalApresVente-this.plusValue)/this.form.quantite;
 
             } else {
-                let coursDollar = this.deviseUSDValue;
+                this.coutTotalAchat = ((this.form.coursAchatAction)*this.form.quantite)+(this.form.fraisAchat); //$
+                this.coutTotalAction = this.coutTotalAchat/this.form.quantite; //$
+                this.investissementTotal = this.form.quantite*this.form.coursActionActuelle //$
+                this.investissementTotalApresVente = (this.investissementTotal-this.form.fraisVente) //$
+                this.plusValue = (this.investissementTotalApresVente-this.coutTotalAchat)/this.form.coursDollar //€
+                console.log(this.plusValue);
 
-                this.coutTotalAchat = ((coursAchatAction)*quantite)+(fraisAchat); //$
-                this.coutTotalAction = this.coutTotalAchat/quantite; //$
-                this.investissementTotal = quantite*coursAction //$
-                this.investissementTotalApresVente = (this.investissementTotal-fraisVente) //$
-                this.plusValue = (this.investissementTotalApresVente-this.coutTotalAchat) //$
-
-                this.value0 = ((this.investissementTotalApresVente-(this.plusValue*(0/100))))/quantite;
-                this.value50 = ((this.investissementTotalApresVente-(this.plusValue*(50/100))))/quantite;
-                this.value100 = ((this.investissementTotalApresVente-this.plusValue))/quantite;
+                this.value0 = (this.investissementTotalApresVente-(this.plusValue*(0/100))*this.form.coursDollar)/this.form.quantite; //$
+                this.value50 = (this.investissementTotalApresVente-(this.plusValue*(50/100))*this.form.coursDollar)/this.form.quantite; //$
+                this.value100 = (this.investissementTotalApresVente-(this.plusValue*this.form.coursDollar))/this.form.quantite; //$
+                console.log(this.plusValue*this.form.coursDollar);
+                console.log(this.plusValue);
             }
         },
-
-        changeUSD(e){
-            console.log(e.target.checked);
-            this.deviseUSDCheckbox = e.target.checked
-        }
     }
 }
 </script>
