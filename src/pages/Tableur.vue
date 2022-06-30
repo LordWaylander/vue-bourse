76fdbf222b99478cac1913f57005e0506bd0b4ce
Waylander <francois.caizes@laposte.net>
2022-06-30 20:09:29 +0200

    update

--- src/pages/Tableur.vue
+++ src/pages/Tableur.vue
@@ -18,8 +18,10 @@
                 <input type="checkbox" id="USDinput" v-model="form.USDinput"/>
             </div>
             <div v-if="form.USDinput" id="USD">
-                <label for="coursDollar">Cours du dollar</label>
-                <input type="text" name="coursDollar" placeholder="Cours du Dollar" v-model="form.coursDollar"/>
+                <label for="coursDollarAchat">Cours du dollar à l'achat</label>
+                <input type="text" name="coursDollarAchat" placeholder="Cours du Dollar à l'achat" v-model="form.coursDollarAchat"/>
+                <label for="coursDollarActuel">Cours du dollar actuellement</label>
+                <input type="text" name="coursDollarActuel" placeholder="Cours du Dollar actuellement" v-model="form.coursDollarActuel"/>
             </div>
             <input type="submit" value="Envoyer" />
         </form>        
@@ -27,24 +29,25 @@
 
     <div id="recap">
         <div v-if="deviseUSD === true" class="values">
-            <p><span class="coutTotalAchat">Cout Total d'achat : </span>{{coutTotalAchat}} $</p>
-            <p><span class="coutTotalAction">Cout Total / action : </span>{{coutTotalAction}} $</p>
-            <p><span class="investissementTotal">Investissement total SANS LES FRAIS DE VENTE: </span>{{investissementTotal}} $</p>
-            <p><span class="investissementTotalApresVente">Investissement total apres vente : </span>{{investissementTotalApresVente}} $</p>
-            <p><span class="plusValue">+/- value : </span>{{plusValue}} €</p>
-            <p><span class="deviseUSD">Taux de change USD : </span>{{form.coursDollar}}</p>
-            
-            <p class="v0"><span>Prix de rachat action à +value 0% : </span>{{value0}} $</p> 
-            <p class="v50"><span>Prix de rachat action à +value 50% : </span>{{value50}} $</p>
-            <p class="v100"><span>Prix de rachat action à +value 100% : </span>{{value100}} $</p>
-        </div>
-        <div v-else-if="deviseUSD === false" class="values">
-            <p><span class="coutTotalAchat">Cout Total d'achat : </span>{{coutTotalAchat}} €</p>
+            <p><span class="coutTotalAchat">Cout Total d'achat (frais achat compris): </span>{{coutTotalAchat}} €</p>
             <p><span class="coutTotalAction">Cout Total / action : </span>{{coutTotalAction}} €</p>
-            <p><span class="investissementTotal">Investissement total SANS LES FRAIS DE VENTE: </span>{{investissementTotal}} €</p>
+            <p style="color: red" ><span class="investissementTotal">Investissement total SANS LES FRAIS DE VENTE: </span>{{investissementTotal}} €</p>
+
             <p><span class="investissementTotalApresVente">Investissement total apres vente : </span>{{investissementTotalApresVente}} €</p>
             <p><span class="plusValue">+/- value : </span>{{plusValue}} €</p>
-            <!--<p><span class="deviseUSD">Taux de change USD : </span>{{deviseUSDValue}}</p>-->
+            <p><span class="deviseUSD">Taux de change USD actuel : </span>{{form.coursDollarActuel}} $</p>
+            
+            <p class="v0"><span>Prix de rachat action à +value 0% : </span>{{value0}} €</p> 
+            <p class="v50"><span>Prix de rachat action à +value 50% : </span>{{value50}} €</p>
+            <p class="v100"><span>Prix de rachat action à +value 100% : </span>{{value100}} €</p>
+        </div>
+        <div v-else-if="deviseUSD === false" class="values">
+            <p><span class="coutTotalAchat">Cout Total d'achat (frais achat compris): </span>{{coutTotalAchat}} €</p>
+            <p><span class="coutTotalAction">Cout Total / action : </span>{{coutTotalAction}} €</p>
+            <p style="color: red"><span class="investissementTotal">Investissement total SANS LES FRAIS DE VENTE: </span>{{investissementTotal}} €</p>
+
+            <p><span class="investissementTotalApresVente">Investissement total apres vente : </span>{{investissementTotalApresVente}} €</p>
+            <p><span class="plusValue">+/- value : </span>{{plusValue}} €</p>
 
             <p class="v0"><span>Prix de rachat action à +value 0% : </span>{{value0}} €</p> 
             <p class="v50"><span>Prix de rachat action à +value 50% : </span>{{value50}} €</p>
@@ -65,12 +68,15 @@ export default {
         return{
             form: {
                 coursAchatAction: '0',
+                coursAchatActionTransition:'',
                 quantite: '0',
                 fraisAchat: '0',
                 fraisVente: '0',
                 coursActionActuelle: '0',
+                coursActionActuelleTransition:'',
                 USDinput: false,
-                coursDollar: '0',
+                coursDollarAchat: '0',
+                coursDollarActuel: '0',
             },
             coutTotalAchat:'',
             coutTotalAction:'',
@@ -81,7 +87,6 @@ export default {
             value50: '',
             value100: '',
             deviseUSD: '',
-            //deviseUSDCheckbox: false,
         } 
     },
     methods: {
@@ -94,32 +99,50 @@ export default {
             this.form.coursActionActuelle =  parseFloat(this.form.coursActionActuelle);
             this.form.coursDollar = parseFloat(this.form.coursDollar);
 
-            if (!this.deviseUSD) {
+            /*if (!this.deviseUSD) {
                 this.coutTotalAchat = (this.form.coursAchatAction*this.form.quantite)+this.form.fraisAchat;
                 this.coutTotalAction = this.coutTotalAchat/this.form.quantite;
                 this.investissementTotal = this.form.quantite*this.form.coursActionActuelle
                 this.investissementTotalApresVente = this.investissementTotal-this.form.fraisVente
                 this.plusValue = this.investissementTotalApresVente-this.coutTotalAchat
 
-                
                 this.value0 = (this.investissementTotalApresVente-(this.plusValue*(0/100)))/this.form.quantite
                 this.value50 = (this.investissementTotalApresVente-(this.plusValue*(50/100)))/this.form.quantite;
                 this.value100 = (this.investissementTotalApresVente-this.plusValue)/this.form.quantite;
 
             } else {
-                this.coutTotalAchat = ((this.form.coursAchatAction)*this.form.quantite)+(this.form.fraisAchat); //$
-                this.coutTotalAction = this.coutTotalAchat/this.form.quantite; //$
-                this.investissementTotal = this.form.quantite*this.form.coursActionActuelle //$
-                this.investissementTotalApresVente = (this.investissementTotal-this.form.fraisVente) //$
-                this.plusValue = (this.investissementTotalApresVente-this.coutTotalAchat)/this.form.coursDollar //€
-                console.log(this.plusValue);
+                this.form.coursAchatActionTransition = this.form.coursAchatAction/this.form.coursDollarAchat;
+                this.form.coursActionActuelleTransition = this.form.coursActionActuelle/this.form.coursDollarActuel;
 
-                this.value0 = (this.investissementTotalApresVente-(this.plusValue*(0/100))*this.form.coursDollar)/this.form.quantite; //$
-                this.value50 = (this.investissementTotalApresVente-(this.plusValue*(50/100))*this.form.coursDollar)/this.form.quantite; //$
-                this.value100 = (this.investissementTotalApresVente-(this.plusValue*this.form.coursDollar))/this.form.quantite; //$
-                console.log(this.plusValue*this.form.coursDollar);
-                console.log(this.plusValue);
+                this.coutTotalAchat = (this.form.coursAchatActionTransition*this.form.quantite+this.form.fraisAchat); // EUR
+                this.coutTotalAction = this.coutTotalAchat/this.form.quantite; //EUR
+                this.investissementTotal = this.form.quantite*this.form.coursActionActuelleTransition; // EUR
+                this.investissementTotalApresVente = (this.investissementTotal-this.form.fraisVente) //EUR
+                this.plusValue = (this.investissementTotalApresVente-this.coutTotalAchat) //EUR
+
+                this.value0 = (this.investissementTotalApresVente-(this.plusValue*(0/100)))/this.form.quantite; // EUR
+                this.value50 = (this.investissementTotalApresVente-(this.plusValue*(50/100)))/this.form.quantite; // EUR
+                this.value100 = (this.investissementTotalApresVente-(this.plusValue))/this.form.quantite; // EUR
+            }*/
+
+            console.log(this.deviseUSD);
+            this.form.coursAchatActionTransition = this.form.coursAchatAction;
+            this.form.coursActionActuelleTransition = this.form.coursActionActuelle;
+            if (this.deviseUSD) {
+                console.log('OK');
+                this.form.coursAchatActionTransition = this.form.coursAchatAction/this.form.coursDollarAchat;
+                this.form.coursActionActuelleTransition = this.form.coursActionActuelle/this.form.coursDollarActuel;
             }
+
+            this.coutTotalAchat = (this.form.coursAchatActionTransition*this.form.quantite)+this.form.fraisAchat;
+            this.coutTotalAction = this.coutTotalAchat/this.form.quantite;
+            this.investissementTotal = this.form.quantite*this.form.coursActionActuelleTransition;
+            this.investissementTotalApresVente = this.investissementTotal-this.form.fraisVente;
+            this.plusValue = this.investissementTotalApresVente-this.coutTotalAchat;
+
+            this.value0 = (this.investissementTotalApresVente-(this.plusValue*(0/100)))/this.form.quantite;
+            this.value50 = (this.investissementTotalApresVente-(this.plusValue*(50/100)))/this.form.quantite;
+            this.value100 = (this.investissementTotalApresVente-this.plusValue)/this.form.quantite;
         },
     }
 }
