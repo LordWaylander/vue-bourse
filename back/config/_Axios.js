8974f931650686b8b09f12d5b55aeb8745549d81
Waylander <90508325+LordWaylander@users.noreply.github.com>
2022-07-29 08:05:14 +0200

    update

--- back/config/_Axios.js
+++ back/config/_Axios.js
@@ -4,6 +4,14 @@ exports.axios = axios.create({
     baseURL:'https://www.alphavantage.co/',
     headers : {
         'Content-Type': 'application/json',
+    },
+    proxy: {
+        host: configEnv.config.HTTP_PROXY_ADDRESS,
+        protocol: 'https',
+        auth: {
+            username: configEnv.config.HTTP_PROXY_USER, 
+            password: configEnv.config.HTTP_PROXY_MDP
+        }
     }
 })
 
