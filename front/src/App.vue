ee0e0b38fb560775415e70c9b6c4bb918f82d80d
Waylander <90508325+LordWaylander@users.noreply.github.com>
2022-08-02 17:54:28 +0200

    Update App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -29,21 +29,39 @@ import jwtDecode from 'vue-jwt-decode'
       return {
         auth : false,
         query : "msft",
-        querySearchIndice : ""
+        querySearchIndice : "",
+        dateNow : ''
       }
     },
     methods: {
       userConnected(payload){
         this.auth = payload.connected
+        if(this.auth === true){
+          // si authentifier lancement fonction validité token
+          this.date();
+        }
       },
       searchIndiceBourse(payload) {
         this.querySearchIndice = payload.valueSearch;
       },
       queryValueConnected(payload) {
         this.query = payload.queryValueConnected
+      },
+      date() {
+        // calcul de date pour vérification validité token
+          setInterval(() => {
+            if(this.auth === true){
+              return this.dateNow =  Math.trunc(Date.now()/1000)
+            }
+          }, 1000);
+        
       }
     },
     created() {
+      /**
+       * mettre en place un check régulier du token
+       * export date exp ds data et watch sur heure actuelle ?
+       */
       
       if (localStorage.token) {
         let dateNow = Math.trunc(Date.now()/1000);
@@ -54,10 +72,26 @@ import jwtDecode from 'vue-jwt-decode'
           localStorage.removeItem('token');
           return this.auth = false
         }
-
+        this.date()
         return this.auth = true
+        
       }
     },
+    watch:{
+      'dateNow'(){
+        /**
+         * déconnexion si token expiré
+         * mettre un avertissement ?
+         */
+        
+        let decodedToken = jwtDecode.decode(localStorage.token);
+        const expDate = decodedToken.exp
+        if(this.dateNow > expDate) {
+          localStorage.removeItem('token');
+          return this.auth = false
+        }
+      }
+    }
   }
 </script>
 
