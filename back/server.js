bcb18838c92038b2b51f1aa562272a240c9c0519
Waylander <francois.caizes@laposte.net>
2022-08-24 16:58:18 +0200

    Update server.js

--- back/server.js
+++ back/server.js
@@ -3,22 +3,28 @@ const autoload = require('@fastify/autoload');
 const path = require('path');
 const {config} = require('./_services/Env.js');
 
+/**
+ * utile uniquement pour postman a 1ere vue
+ */
+ fastify.register(require('@fastify/formbody'));
+ fastify.register(require('@fastify/multipart'));
+
 // connection à MongoDB
 fastify.register(require('@fastify/mongodb'), {
     forceClose: true,
     url: 'mongodb://localhost:27017/'
 })
 
-fastify.listen(config.SERVER_PORT)
-.then((address) => {
-    fastify.log.info('ADDR Serveur '+address);
-})
+fastify.listen({port: config.SERVER_PORT})
 .catch((err)=> {
-    fastify.log.error(err), process.exit(1)
+    console.log(err), process.exit(1)
 })
 
 fastify.register(require('@fastify/cors'), {
-  // ces deux opts a true avec axios et les credentials à true
+  /**
+   * ces deux opts a true avec axios et les credentials à true
+   * Permet de set des cookies dans la requête
+   */
   origin: true,
   credentials: true,
 });
@@ -26,12 +32,6 @@ fastify.register(require('@fastify/cookie'), {
     secret: config.COOKIE_SECRET,
     parseOptions: {}
 })
-
-/**
- * utile uniquement pour postman a 1ere vue
- */
-fastify.register(require('@fastify/formbody'));
-fastify.register(require('@fastify/multipart'));
 
 //visions des routes ouvertes à definir AVANT l'initialisation des routes
 fastify.addHook('onRoute', (routeOptions) => {
