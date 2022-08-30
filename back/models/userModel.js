8054a87bfce24c5106f04eb811724b41345f0b10
Waylander <francois.caizes@laposte.net>
2022-08-31 00:25:28 +0200

    Update userModel.js

--- back/models/userModel.js
+++ back/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Achat = require('./achatModel')
+const {Achat} = require('./achatModel')
 
 const userSchema = new Schema({
   prenom: {
